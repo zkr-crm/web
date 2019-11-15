1979b07ddcafd981c371ce179411662c5443c6da
sinosofthongxu <275899872@qq.com>
2019-11-15 16:26:36 +0800

    客群新增客户

--- src/app/pages/custGroup/popupPage/showCustListCtrl.js
+++ src/app/pages/custGroup/popupPage/showCustListCtrl.js
@@ -1,69 +1,72 @@
 (function() {
 	'use strict';
 
-	angular.module('BlurAdmin.pages.custGroup').controller('showCustListCtrl',
-			showCustListCtrl);
+	angular.module('BlurAdmin.pages.custGroup').controller('groupSelectCustListCtrl',
+		groupSelectCustListCtrl);
 	/** @ngInject */
-	function showCustListCtrl($scope, $state, HttpService, EnumType, Alert,
-			$uibModal, $rootScope) {
-
-		var custAgentList = [];
-
-		var initOpts = {};
-		initOpts.url = '/crm/manage/auth/getRoleDateAuth';
-		initOpts.method = 'GET';
-		initOpts.params = {
-			userCode : $rootScope.global.user,
-			tableCode : "T001"
+	function groupSelectCustListCtrl($scope, HttpService, EnumType, Alert, $rootScope) {
+		$scope.checkedRow = []; //选中的客户数组
+		$scope.searchObj = {}; //查询客户列表的条件
+		$scope.smartTablePageSize = 5; // 页面大小
+		/**
+		 * [search 客户列表查询]
+		 * @param  {[type]} page [页码]
+		 * @return {[type]}      [description]
+		 */
+		$scope.search = function(page) {
+			$scope.queryPage(page);
+		}
+		// 单个选中
+		$scope.selectOne1 = function(item) {
+			$scope.checkedRow = []
+			angular.forEach($scope.custCollection, function(i) {
+				var index = $scope.checkedRow.indexOf(i);
+				if (i.checked) {
+					$scope.checkedRow.push(i);
+				}
+			});
+		}
+		// 点击行选中本行
+		$scope.selectRow = function(item) {
+			item.checked = !item.checked;
+			$scope.selectOne1(item)
+		}
+		//多选
+		$scope.selectAll1 = function(e) {
+			if (e) {
+				$scope.checkedRow = [];
+				var count = 0;
+				angular.forEach($scope.custCollection, function(cust) {
+					cust.checked = true;
+					// 选中的客户
+					var checkedCust = {
+						'custNo': cust.custNo,
+						'custTyp': cust.custTyp
+					};
+					$scope.checkedRow.push($scope.checkedCust);
+				})
+				$scope.select_all = true;
+			} else {
+				angular.forEach($scope.custCollection, function(cust) {
+					cust.checked = false;
+				})
+				$scope.checkedRow = [];
+				$scope.select_all = false;
+			}
 		};
-		HttpService
-				.linkHttp(initOpts)
-				.then(
-						function(result) {
-
-							if (!(result.data === undefined || result.data.length == 0)) {
-
-								angular.forEach(result.data, function(i) {
-									if (i.employeeId !== undefined) {
-										custAgentList.push(i.employeeId);
-									}
-								});
-
-								var opts = {};
-								opts.url = '/crm/ecif/cust/getPerCustListByRole';
-								opts.method = 'POST';
-								opts.params = {};
-								opts.data = custAgentList;
-								HttpService
-										.linkHttp(opts)
-										.then(
-												function(response) {
-													$scope.custCollection = response.data;
-													$scope.custCollection = response.data
-															.map(function(item) {
-																item.custTypNam = EnumType.CustType
-																		.getLabelByValue(item.custTyp);
-																item.custSourceNam = EnumType.DataSource
-																		.getLabelByValue(item.custSource);
-																item.certTypNam = EnumType.IdType
-																		.getLabelByValue(item.certTyp);
-																return item;
-															});
-													$scope.total = response.data.length;
-												});
-							}
-						});
-
 		// 添加群组成员
 		$scope.addGroupMember = function() {
-
+			if ($scope.checkedRow.length == 0) {
+				Alert.error('请选择用户！');
+				return;
+			}
 			var groupMemberObj = [];
 			angular.forEach($scope.checkedRow, function(item) {
 				var obj = {};
 				obj.groupId = $scope.groupId;
 				obj.custNo = item.custNo;
 				obj.custType = item.custTyp;
-				obj.groupType = EnumType.CustGroupTyp.staticGroup.value;// 静态群组
+				obj.groupType = EnumType.CustGroupTyp.staticGroup.value; // 静态群组
 				groupMemberObj.push(obj);
 			});
 
@@ -73,66 +76,75 @@
 			opts.params = {};
 			opts.data = groupMemberObj;
 			HttpService.linkHttp(opts).then(function(response) {
-
-				Alert.success("添加群组成员成功,添加成员个数：[" + response.data + "]!");
+				if (response.status === '1') {
+					if (response.data > 0) {
+						//Alert.success("部分客户已存在,添加成员个数：[" + (groupMemberObj.length-response.data) + "]!");
+						Alert.success("部分客户已存在,添加成功!");
+					} else {
+						//Alert.success("添加群组成员成功,添加成员个数：[" + groupMemberObj.length + "]!");
+						Alert.success("添加群组成员成功!");
+					}
+					$scope.addGroupMemberModal.close();
+				} else {
+					Alert.error(response.message)
+				}
 				$scope.initData();
 			});
 		}
-
-		// 查询条件对象
-		$scope.searchObj = {
-			'custAgent' : '',
-			'custName' : ''
-		};
-		// 查询事件
-		$scope.searchUser = function() {
-
-			$scope.queryCustList.params = $scope.searchObj;
-			this.queryPage(1);
-		}
-
-		// 选中行对象
-		$scope.checkedRow = [];
-		// 全选
-		$scope.selectAll = function() {
-			if (!$scope.select_all) {
-				$scope.checkedRow = [];
-				var count = 0;
-				angular.forEach($scope.custCollection, function(i) {
-					i.isChecked = true;
-
-					// 选中对象
-					$scope.checkedRow.push(i);
-				})
-				$scope.select_all = true;
-			} else {
-				angular.forEach($scope.custCollection, function(i) {
-					i.isChecked = false;
-					$scope.checkedRow = [];
-				})
-				$scope.select_all = false;
-			}
-		};
-
-		// 单个选中
-		$scope.selectOne = function() {
-			angular.forEach($scope.custCollection, function(i) {
-				var index = $scope.checkedRow.indexOf(i);
-				if (i.isChecked && index === -1) {
-
-					// 选中对象
-					$scope.checkedRow.push(i);
-				} else if (!i.isChecked && index !== -1) {
-					$scope.checkedRow.splice(index, 1);
+		// 初始化方法
+		var init = function() {
+			$scope.$on('queryPage', function(event, queryPage) {
+				if (!$scope.queryPage) {
+					$scope.queryPage = queryPage;
 				}
 			});
-
-			if ($scope.custCollection.length === $scope.checkedRow.length) {
-				$scope.select_all = true;
-			} else {
-				$scope.select_all = false;
+			//用户权限查询
+			var custAgentList = []; //客户经理数组，数据权限控制
+			var initOpts = {};
+			initOpts.url = '/crm/manage/auth/getRoleDateAuth';
+			initOpts.method = 'GET';
+			initOpts.params = {
+				userCode: $rootScope.global.user,
+				tableCode: "T001"
+			};
+			HttpService
+				.linkHttp(initOpts)
+				.then(
+					function(result) {
+						if (!(result.data === undefined || result.data.length == 0)) {
+							angular.forEach(result.data, function(i) {
+								if (i.employeeId !== undefined) {
+									custAgentList.push(i.employeeId);
+								}
+							});
+						}
+						$scope.queryPage();
+					});
+			//分页查询客户信息列表
+			$scope.queryCustList = {};
+			$scope.queryCustList.pagination = {
+				pageSize: '10',
+				pageIndex: 1,
+				maxText: 5
+			}
+			$scope.queryCustList.noDefaultQuery = true;//是否默认查询
+			$scope.queryCustList.url = '/crm/ecif/cust/getPerCustListByRole';
+			$scope.queryCustList.method = 'POST';
+			$scope.queryCustList.params = $scope.searchObj;
+			$scope.queryCustList.data = custAgentList; //用户数据权限
+			$scope.queryCustList.success = function successCallback(response) {
+				if (response.status === '1') {
+					$scope.custCollection = response.data.list;
+					$scope.custCollection = response.data.list.map(function(item) {
+						item.custTypNam = EnumType.CustType.getLabelByValue(item.custTyp);
+						item.custSourceNam = EnumType.DataSource.getLabelByValue(item.custSource);
+						item.certTypNam = EnumType.IdType.getLabelByValue(item.certTyp);
+						return item;
+					});
+					$scope.total = response.data.length;
+				}
 			}
 		}
+		init();
 	}
-
 })();
